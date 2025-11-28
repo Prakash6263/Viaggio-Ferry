baa23fb93270c4ab2eada5167e49ce7cc4d28533
Prakash6263 <prakashmishra6263@gmail.com>
2025-11-28 16:06:57 +0530

    superadmin

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const companyRoutes = require("./src/routes/companyRoutes")
 const app = express()
 
 // Security & parsing
-app.use(helmet())
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  }),
+)
 app.use(cors())
 app.use(express.json({ limit: "1mb" }))
 app.use(express.urlencoded({ extended: true }))
