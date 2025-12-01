5dcd229475dec34b1dc22b926fd43967ba870d42
Prakash6263 <prakashmishra6263@gmail.com>
2025-12-01 10:27:54 +0530

    superadmin

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
