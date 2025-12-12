ad3cda428951a8d21c545d3bca69b7f8734c4e89
Prakash6263 <prakashmishra6263@gmail.com>
2025-12-12 10:55:31 +0530

    add cors

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ const app = express()
 
 // Security & parsing
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "https://voyagian.com",
+      "https://admin.voyagian.com",
+      "https://company.voyagian.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
+)
+
 app.use(express.json({ limit: "1mb" }))
 app.use(express.urlencoded({ extended: true }))
 
